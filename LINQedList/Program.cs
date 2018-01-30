f2c33bfbbef09edbf4f426fdfc5780cb37095ae9
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-01-29 21:15:13 -0600

    next one working

--- LINQedList/Program.cs
+++ LINQedList/Program.cs
@@ -72,6 +72,40 @@ namespace LINQedList
                 Console.WriteLine(number);
             }
             Console.ReadLine();
+
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var sum = numbers3.Count();
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            var sum2 = purchases.Sum(purchase => purchase);
+
+            Console.WriteLine(sum2);
+            Console.ReadLine();
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            var maxs = prices.Max(price => price);
+
+            Console.WriteLine(maxs);
+            Console.ReadLine();
         }
     }
 }
